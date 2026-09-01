3a188d9b179dc6afa6e2953e9268006518611271
Pedro Olivier <pedro0olivier@gmail.com>
2026-09-01 02:01:09 -0300

    Add temperature conversion from Celsius to Fahrenheit

---
+++ ex02.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a temperatura em Celsius: ");
+        double celsius = scanner.nextDouble();
+
+        double fahrenheit = (celsius * 1.8) + 32;
+
+        System.out.println("Temperatura em Fahrenheit: " + fahrenheit);
+
+        scanner.close();
+    }
+}
